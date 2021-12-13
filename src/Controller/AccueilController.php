4015565510b24acb77de5ba82a8977cf0ddad58f
Mocky-FS <arnaud.solier@outlook.fr>
2021-12-13 23:21:39 +0100

    Phase 3 : introduction à Twig et simplification du contrôleur.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,20 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 /**
  * Description of AccueilController
  * 
  * @author Mocky
  */
-class AccueilController {
+class AccueilController extends AbstractController {
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
-    public function index (): Response{
-        return new Response('Hello World!');
+    public function index(): Response{
+        return $this->render("pages/accueil.html.twig");
     }
+   
+    
 }
